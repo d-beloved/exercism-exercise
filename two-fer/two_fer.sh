e938256b2f9039b4d1962ced09b1ea305ddc499b
d-beloved <morayodeji@gmail.com>
2020-12-06 21:15:11 +0100

    edit per mentor comment

--- two-fer/two_fer.sh
+++ two-fer/two_fer.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
 main () {
-  # your main function code here
   if [[ $1 ]]; then
     echo "One for $1, one for me."
   else
@@ -23,7 +8,5 @@ main () {
   fi
 }
 
-# call main with all of the positional arguments
 main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+
